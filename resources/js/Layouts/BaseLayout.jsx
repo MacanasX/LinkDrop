55ad82930d97d762becfb996b6c73ed5a6bc0cf0
Luca Groß <gross-darmstadt@web.de>
2025-07-05 14:27:27 +0200

    Added Favicons

--- resources/js/Layouts/BaseLayout.jsx
+++ resources/js/Layouts/BaseLayout.jsx
@@ -17,6 +17,24 @@ export function BaseLayout({
                 ) : null}
                 {robots && <meta name='robots' content={robots} />}
                 {canonical && <link rel='canonical' href={canonical} />}
+                <link rel='icon' href='/favicon.ico' />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/favicon-16x16.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/favicon-32x32.png'
+                />
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='/apple-touch-icon.png'
+                />
             </Head>
             {children}
         </Fragment>
